842d95890408206e31b2fd201af125fb2a009c72
Samuel Adewole <samueladewole15@gmail.com>
2020-01-04 10:35:50 +0100

    setup server and client

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require('express')
-const socket = require('socket.io')
+const socketio = require('socket.io')
 const cors = require('cors')
+const http = require('http')
 
+const router = require('./router')
 const app = express()
+
+// create cross origin 
+app.use(cors())
+// middlewares
+const server = http.createServer(app)
+const io = socketio(server)
+
+io.on('connection', (socket) => {
+    console.log('we have a new connection')
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected')
+    })
+})
+
+// route
+app.use(router)
 
 const PORT = process.env.PORT || 5500
 
-app.listen(PORT, ()=> console.log(`App running on PORT: ${PORT}`))
+server.listen(PORT, () => console.log(`App running on PORT: ${PORT}`))
